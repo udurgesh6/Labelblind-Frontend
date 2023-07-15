6d0f5d7aa31c62145854c3adfe49fddf448ff7d7
Durgesh Upadhyay <udurgesh6@gmail.com>
2023-07-15 14:45:22 +0530

    scrapped data

--- src/Scrape.tsx
+++ src/Scrape.tsx
@@ -1,18 +1,24 @@
-import React, { useState, ChangeEvent, FormEvent } from 'react';
+import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react';
 import axios from 'axios';
 
 const Scrape: React.FC = () => {
   const [url, setUrl] = useState<string>('');
-
+  const [data, setData] = useState<string>('');
   const scrapeData = () => {
     axios
-      .get(`https://labelblind-backend.vercel.app/scrape?url=${url}`)
+      .get(`https://labelblind-backend.vercel.app/scrape?url=${encodeURI(url)}`)
       .then(() => {
         alert('Data scraped successfully');
         setUrl('');
       })
       .catch(() => alert('Some error occurred while scraping the data'));
   };
+
+  useEffect(() => {
+    axios
+      .get('https://labelblind-backend.vercel.app/data')
+      .then(resp => setData(resp.data));
+  }, []);
 
   const handleUrlChange = (e: ChangeEvent<HTMLInputElement>) => {
     setUrl(e.target.value);
@@ -24,10 +30,18 @@ const Scrape: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="flex flex-col">
       <label>Enter URL to scrape</label>
-      <input value={url} onChange={handleUrlChange} />
-      <button type="submit">Scrape</button>
+      <input
+        value={url}
+        onChange={handleUrlChange}
+        className="border border-gray-900 w-[200px]"
+      />
+      <button type="submit" className="border border-gray-800 bg-900 w-[200px]">
+        Scrape
+      </button>
+      <label>Data Till Now</label>
+      {JSON.stringify(data)}
     </form>
   );
 };
